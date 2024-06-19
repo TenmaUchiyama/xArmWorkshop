a927ab9f8212e67bc16779c0afa202fb3fff34d5
KaitoKurokochi <106721539+KaitoKurokochi@users.noreply.github.com>
2024-06-19 13:34:13 +0900

    Update Exercise1.py

--- Exercise/Exercise1.py
+++ Exercise/Exercise1.py
@@ -58,24 +58,24 @@ def main():
         print("座標はワークスペース外です。")
 
 
-    z += 500
+    z += 300
     if CheckIfNewPositionInWorkspace(x,y,z): #新しい値がworkspaceに存在するかを調べる。
         arm.set_position(x,y,z, speed= speed, wait=True ) #ポジションを指定する。 
     time.sleep(1) #移動後に1秒待つ。
 
   
-    y += 500
+    y += 300
     if CheckIfNewPositionInWorkspace(x,y,z): #新しい値がworkspaceに存在するかを調べる。
         arm.set_position(x,y,z, speed= speed, wait=True ) #ポジションを指定する。
     time.sleep(1) #移動後に1秒待つ。
 
    
-    z -= 500
+    z -= 300
     if CheckIfNewPositionInWorkspace(x,y,z): #新しい値がworkspaceに存在するかを調べる。
         arm.set_position(x,y,z, speed= speed, wait=True ) #ポジションを指定する。
     time.sleep(1) #移動後に1秒待つ。
 
-    z -= 500
+    z -= 300
     if CheckIfNewPositionInWorkspace(x,y,z): #新しい値がworkspaceに存在するかを調べる。
         arm.set_position(x,y,z, speed= speed, wait=True ) #ポジションを指定する。
     time.sleep(1) #移動後に1秒待つ。
